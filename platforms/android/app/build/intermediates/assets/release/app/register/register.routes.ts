2e2b276f1b40485d8f84be9282156bec4c5e6abc
Tunde Kadiri <tunde.kadiri96@gmail.com>
2018-05-03 15:53:41 -0700

    created new apk for deployment

--- platforms/android/app/build/intermediates/assets/release/app/register/register.routes.ts
+++
@@ -1,9 +0,0 @@
-import { ModuleWithProviders }  from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-
-import { RegisterComponent } from "./register.component";
-
-const registerRoutes: Routes = [
-  { path: "", component: RegisterComponent },
-];
-export const registerRouting: ModuleWithProviders = RouterModule.forChild(registerRoutes);
--- platforms/android/app/build/intermediates/assets/release/app/services/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./backend.service";
-export * from "./firebase.service";
-export * from "./utils.service";
-export * from "./firebase.service.1";
